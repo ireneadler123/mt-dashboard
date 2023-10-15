47d04e270aaa58828633de10b8325a9e812709d9
ireneadler123 <145485199+ireneadler123@users.noreply.github.com>
2023-10-16 00:48:07 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -460,8 +460,7 @@ elif pages == 'Tăng trưởng lũy kế':
     with middle:
         if kind == 'QTD - YOY %':
             # df_past = df1[df1['Hệ thống'] == supermarket]
-            df_past = df[(df['Ngày lấy đơn'].astype('datetime64[ns]') >= (date1 - timedelta(days = 365))) & (pd.to_datetime(df['Ngày lấy đơn'].astype('datetime64[ns]'), dayfirst = True) <= (date2 - timedelta(days = 365)))]
-            df_past['Ngày lấy đơn'] = df_past['Ngày lấy đơn'].astype('str').str.split(' ')
+            df_past = df[(pd.to_datetime(df['Ngày lấy đơn'].astype('datetime64[ns]'), dayfirst = True) >= (date1 - timedelta(days = 365))) & (pd.to_datetime(df['Ngày lấy đơn'].astype('datetime64[ns]'), dayfirst = True) <= (date2 - timedelta(days = 365)))]            df_past['Ngày lấy đơn'] = df_past['Ngày lấy đơn'].astype('str').str.split(' ')
             df_past['Ngày lấy đơn'] = df_past['Ngày lấy đơn'].agg({lambda x: x[0]})
             quality = df1[(pd.to_datetime(df1['Ngày lấy đơn'], dayfirst = True) >= date1) & (pd.to_datetime(df1['Ngày lấy đơn'], dayfirst = True) <= date2)].copy()
             quality = df1.groupby(by = 'Mã đơn hàng').agg({'Hàng bán (Thùng)': 'sum'}).reset_index()
